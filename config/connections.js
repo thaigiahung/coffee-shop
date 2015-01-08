cf62c85e65fbd4e1abb777d6c2cb8aa7c2c8cb15
chitrungit <MacMini@MacMini.local>
2015-01-08 10:47:55 +0700

    Change DB name

--- config/connections.js
+++ config/connections.js
@@ -45,7 +45,7 @@ module.exports.connections = {
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'inPOS'
+    database: 'erp'
   },
   // mysql: {
   //   module    : 'sails-mysql',
